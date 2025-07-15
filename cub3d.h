36d6795a3acf2e218d236e3191d4920da1cf8dee
artix31 <joyofcreation22@hotmail.com>
2025-07-15 22:57:27 +0300

    started base, added instructions, made a, kinda, working mlx, still needs a lot of, well, code

---
+++ cub3d.h
@@ -0,0 +1,33 @@
+# ifndef CUB3D_H
+# define CUB3D_H
+#define SCREEN_WIDTH 1200
+#define SCREEN_HEIGHT 800
+#include <mlx.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+Map struct, has coordinates for the map, and visuals inside it
+*/
+typedef struct s_map{
+	char	**map;
+	long	x;
+	long	y;
+}	t_map;
+
+typedef struct s_screen{
+	;
+}	t_screen;
+
+/*
+main struct, has mlx and is parent of all structs
+*/
+typedef struct s_data{
+	void		*mlx;
+	void		*window;
+	t_map		*map;
+	t_screen	*screen;
+}	t_data;
+
+# endif
